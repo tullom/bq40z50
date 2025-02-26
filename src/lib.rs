5ae372524e86b0993171b8085e1054c6b72c1565
matteotullo <matteotullo@microsoft.com>
2025-02-26 08:58:03 -0800

    Add embedded-batteries traits and From trait conversions

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,12 @@
 #![cfg_attr(not(test), no_std)]
 #![allow(missing_docs)]
 
+use core::cell::Cell;
+
+use embedded_batteries_async::smart_battery::{
+    self, BatteryModeFields, BatteryStatusFields, CapacityModeSignedValue, CapacityModeValue, DeciKelvin, ErrorCode,
+    SpecificationInfoFields,
+};
 use embedded_hal_async::i2c::I2c as I2cTrait;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
@@ -19,6 +25,7 @@ use embedded_hal_async::i2c::I2c as I2cTrait;
 /// BQ40Z50 Errors
 pub enum BQ40Z50Error<I2cError> {
     I2c(I2cError),
+    BatteryStatus(ErrorCode),
 }
 
 const BQ_ADDR: u8 = 0x0B;
@@ -28,10 +35,53 @@ const LARGEST_BUF_SIZE_BYTES: usize = 33;
 const MAC_CMD_ADDR_SIZE_BYTES: usize = 2;
 const MAC_CMD_ADDR_SIZE_BITS: usize = MAC_CMD_ADDR_SIZE_BYTES * 8;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum CapacityModeState {
+    Milliamps = 0,
+    Centiwatt = 1,
+}
+
+impl From<field_sets::BatteryStatus> for BatteryStatusFields {
+    fn from(value: field_sets::BatteryStatus) -> Self {
+        BatteryStatusFields::new()
+            .with_error_code(value.ec())
+            .with_fully_discharged(value.fd())
+            .with_fully_charged(value.fc())
+            .with_discharging(value.dsg())
+            .with_initialized(value.init())
+            .with_remaining_time_alarm(value.rta())
+            .with_remaining_capacity_alarm(value.rca())
+            .with_terminate_discharge_alarm(value.tda())
+            .with_over_temp_alarm(value.ota())
+            .with_terminate_charge_alarm(value.tca())
+            .with_over_charged_alarm(value.oca())
+    }
+}
+
+impl From<field_sets::BatteryMode> for BatteryModeFields {
+    fn from(value: field_sets::BatteryMode) -> Self {
+        let battery_mode_raw: [u8; 2] = value.into();
+        u16::from_le_bytes(battery_mode_raw).into()
+    }
+}
+
+impl From<BatteryModeFields> for field_sets::BatteryMode {
+    fn from(value: BatteryModeFields) -> Self {
+        u16::from(value).to_le_bytes().into()
+    }
+}
+
+impl From<field_sets::SpecificationInfo> for SpecificationInfoFields {
+    fn from(value: field_sets::SpecificationInfo) -> Self {
+        let spec_info_raw: [u8; 2] = value.into();
+        u16::from_le_bytes(spec_info_raw).into()
+    }
+}
+
 /// BQ40Z50 interface, which takes an async I2C bus
-pub struct DeviceInterface<I2c: I2cTrait> {
+pub struct DeviceInterface<I2C: I2cTrait> {
     /// embedded-hal-async compliant I2C bus
-    pub i2c: I2c,
+    pub i2c: I2C,
 }
 
 device_driver::create_device!(
@@ -39,8 +89,8 @@ device_driver::create_device!(
     manifest: "device.yaml"
 );
 
-impl<I2c: I2cTrait> device_driver::AsyncRegisterInterface for DeviceInterface<I2c> {
-    type Error = BQ40Z50Error<I2c::Error>;
+impl<I2C: I2cTrait> device_driver::AsyncRegisterInterface for DeviceInterface<I2C> {
+    type Error = BQ40Z50Error<I2C::Error>;
     type AddressType = u8;
 
     async fn write_register(
@@ -78,8 +128,8 @@ impl<I2c: I2cTrait> device_driver::AsyncRegisterInterface for DeviceInterface<I2
     }
 }
 
-impl<I2c: I2cTrait> device_driver::AsyncCommandInterface for DeviceInterface<I2c> {
-    type Error = BQ40Z50Error<I2c::Error>;
+impl<I2C: I2cTrait> device_driver::AsyncCommandInterface for DeviceInterface<I2C> {
+    type Error = BQ40Z50Error<I2C::Error>;
     type AddressType = u32;
 
     async fn dispatch_command(
@@ -104,11 +154,11 @@ impl<I2c: I2cTrait> device_driver::AsyncCommandInterface for DeviceInterface<I2c
     }
 }
 
-impl<I2c: I2cTrait> device_driver::BufferInterfaceError for DeviceInterface<I2c> {
-    type Error = BQ40Z50Error<I2c::Error>;
+impl<I2C: I2cTrait> device_driver::BufferInterfaceError for DeviceInterface<I2C> {
+    type Error = BQ40Z50Error<I2C::Error>;
 }
 
-impl<I2c: I2cTrait> device_driver::AsyncBufferInterface for DeviceInterface<I2c> {
+impl<I2C: I2cTrait> device_driver::AsyncBufferInterface for DeviceInterface<I2C> {
     type AddressType = u8;
 
     async fn read(&mut self, address: Self::AddressType, buf: &mut [u8]) -> Result<usize, Self::Error> {
@@ -132,6 +182,303 @@ impl<I2c: I2cTrait> device_driver::AsyncBufferInterface for DeviceInterface<I2c>
 
     async fn flush(&mut self, _address: Self::AddressType) -> Result<(), Self::Error> {
         Ok(())
+    }
+}
+
+impl<E: embedded_hal_async::i2c::Error> smart_battery::Error for BQ40Z50Error<E> {
+    fn kind(&self) -> smart_battery::ErrorKind {
+        match self {
+            Self::I2c(_) => smart_battery::ErrorKind::CommError,
+            Self::BatteryStatus(e) => smart_battery::ErrorKind::BatteryStatus(*e),
+        }
+    }
+}
+
+pub struct Bq40z50<I2C: I2cTrait> {
+    pub device: Device<DeviceInterface<I2C>>,
+    capacity_mode_state: Cell<CapacityModeState>,
+}
+
+impl<I2C: I2cTrait> Bq40z50<I2C> {
+    pub fn new(i2c: I2C) -> Self {
+        Bq40z50 {
+            device: Device::new(DeviceInterface { i2c }),
+            capacity_mode_state: Cell::new(CapacityModeState::Milliamps),
+        }
+    }
+
+    fn set_capacity_mode_state(&self, battery_mode_fields: BatteryModeFields) {
+        self.capacity_mode_state.set(if battery_mode_fields.capacity_mode() {
+            CapacityModeState::Centiwatt
+        } else {
+            CapacityModeState::Milliamps
+        });
+    }
+}
+
+impl<I2C: I2cTrait> smart_battery::ErrorType for Bq40z50<I2C> {
+    type Error = BQ40Z50Error<I2C::Error>;
+}
+
+impl<I2C: I2cTrait> smart_battery::SmartBattery for Bq40z50<I2C> {
+    async fn remaining_capacity_alarm(&mut self) -> Result<smart_battery::CapacityModeValue, Self::Error> {
+        Ok(match self.capacity_mode_state.get() {
+            CapacityModeState::Milliamps => smart_battery::CapacityModeValue::MilliAmpUnsigned(
+                self.device
+                    .remaining_capacity_alarm()
+                    .read_async()
+                    .await?
+                    .remaining_capacity_alarm(),
+            ),
+            CapacityModeState::Centiwatt => smart_battery::CapacityModeValue::CentiWattUnsigned(
+                self.device
+                    .remaining_capacity_alarm()
+                    .read_async()
+                    .await?
+                    .remaining_capacity_alarm(),
+            ),
+        })
+    }
+
+    async fn set_remaining_capacity_alarm(
+        &mut self,
+        capacity: smart_battery::CapacityModeValue,
+    ) -> Result<(), Self::Error> {
+        self.device
+            .remaining_capacity_alarm()
+            .write_async(|d| {
+                d.set_remaining_capacity_alarm(match capacity {
+                    CapacityModeValue::MilliAmpUnsigned(value) | CapacityModeValue::CentiWattUnsigned(value) => value,
+                });
+            })
+            .await
+    }
+
+    async fn remaining_time_alarm(&mut self) -> Result<smart_battery::Minutes, Self::Error> {
+        Ok(self
+            .device
+            .remaining_time_alarm()
+            .read_async()
+            .await?
+            .remaining_time_alarm())
+    }
+
+    async fn set_remaining_time_alarm(&mut self, time: smart_battery::Minutes) -> Result<(), Self::Error> {
+        self.device
+            .remaining_time_alarm()
+            .write_async(|f| f.set_remaining_time_alarm(time))
+            .await
+    }
+
+    async fn battery_mode(&mut self) -> Result<BatteryModeFields, Self::Error> {
+        Ok(self.device.battery_mode().read_async().await?.into())
+    }
+
+    async fn set_battery_mode(&mut self, flags: BatteryModeFields) -> Result<(), Self::Error> {
+        self.set_capacity_mode_state(flags);
+        self.device.battery_mode().write_async(|f| *f = flags.into()).await
+    }
+
+    async fn at_rate(&mut self) -> Result<smart_battery::CapacityModeSignedValue, Self::Error> {
+        Ok(match self.capacity_mode_state.get() {
+            CapacityModeState::Milliamps => smart_battery::CapacityModeSignedValue::MilliAmpSigned(
+                self.device.at_rate().read_async().await?.at_rate(),
+            ),
+            CapacityModeState::Centiwatt => smart_battery::CapacityModeSignedValue::CentiWattSigned(
+                self.device.at_rate().read_async().await?.at_rate(),
+            ),
+        })
+    }
+
+    async fn set_at_rate(&mut self, rate: smart_battery::CapacityModeSignedValue) -> Result<(), Self::Error> {
+        self.device
+            .at_rate()
+            .write_async(|f| {
+                f.set_at_rate(match rate {
+                    CapacityModeSignedValue::MilliAmpSigned(value)
+                    | CapacityModeSignedValue::CentiWattSigned(value) => value,
+                });
+            })
+            .await
+    }
+
+    async fn at_rate_time_to_full(&mut self) -> Result<smart_battery::Minutes, Self::Error> {
+        Ok(self
+            .device
+            .at_rate_time_to_full()
+            .read_async()
+            .await?
+            .at_rate_time_to_full())
+    }
+
+    async fn at_rate_time_to_empty(&mut self) -> Result<smart_battery::Minutes, Self::Error> {
+        Ok(self
+            .device
+            .at_rate_time_to_empty()
+            .read_async()
+            .await?
+            .at_rate_time_to_empty())
+    }
+
+    async fn at_rate_ok(&mut self) -> Result<bool, Self::Error> {
+        // 0 = false, anything else is true
+        Ok(!matches!(self.device.at_rate_ok().read_async().await?.at_rate_ok(), 0))
+    }
+
+    async fn temperature(&mut self) -> Result<DeciKelvin, Self::Error> {
+        Ok(self.device.temperature().read_async().await?.temperature())
+    }
+
+    async fn voltage(&mut self) -> Result<smart_battery::MilliVolts, Self::Error> {
+        Ok(self.device.voltage().read_async().await?.voltage())
+    }
+
+    async fn current(&mut self) -> Result<smart_battery::MilliAmpsSigned, Self::Error> {
+        Ok(self.device.current().read_async().await?.current())
+    }
+
+    async fn average_current(&mut self) -> Result<smart_battery::MilliAmpsSigned, Self::Error> {
+        Ok(self.device.avg_current().read_async().await?.avg_current())
+    }
+
+    async fn max_error(&mut self) -> Result<smart_battery::Percent, Self::Error> {
+        Ok(self.device.max_error().read_async().await?.max_error())
+    }
+
+    async fn relative_state_of_charge(&mut self) -> Result<smart_battery::Percent, Self::Error> {
+        Ok(self
+            .device
+            .relative_state_of_charge()
+            .read_async()
+            .await?
+            .relative_state_of_charge())
+    }
+
+    async fn absolute_state_of_charge(&mut self) -> Result<smart_battery::Percent, Self::Error> {
+        Ok(self
+            .device
+            .absolute_state_of_charge()
+            .read_async()
+            .await?
+            .absolute_state_of_charge())
+    }
+
+    async fn remaining_capacity(&mut self) -> Result<smart_battery::CapacityModeValue, Self::Error> {
+        Ok(match self.capacity_mode_state.get() {
+            CapacityModeState::Milliamps => smart_battery::CapacityModeValue::MilliAmpUnsigned(
+                self.device
+                    .remaining_capacity()
+                    .read_async()
+                    .await?
+                    .remaining_capacity(),
+            ),
+            CapacityModeState::Centiwatt => smart_battery::CapacityModeValue::CentiWattUnsigned(
+                self.device
+                    .remaining_capacity()
+                    .read_async()
+                    .await?
+                    .remaining_capacity(),
+            ),
+        })
+    }
+
+    async fn full_charge_capacity(&mut self) -> Result<smart_battery::CapacityModeValue, Self::Error> {
+        Ok(match self.capacity_mode_state.get() {
+            CapacityModeState::Milliamps => smart_battery::CapacityModeValue::MilliAmpUnsigned(
+                self.device
+                    .full_charge_capacity()
+                    .read_async()
+                    .await?
+                    .full_charge_capacity(),
+            ),
+            CapacityModeState::Centiwatt => smart_battery::CapacityModeValue::CentiWattUnsigned(
+                self.device
+                    .full_charge_capacity()
+                    .read_async()
+                    .await?
+                    .full_charge_capacity(),
+            ),
+        })
+    }
+
+    async fn run_time_to_empty(&mut self) -> Result<smart_battery::Minutes, Self::Error> {
+        Ok(self.device.run_time_to_empty().read_async().await?.run_time_to_empty())
+    }
+
+    async fn average_time_to_empty(&mut self) -> Result<smart_battery::Minutes, Self::Error> {
+        Ok(self
+            .device
+            .average_time_to_empty()
+            .read_async()
+            .await?
+            .average_time_to_empty())
+    }
+
+    async fn average_time_to_full(&mut self) -> Result<smart_battery::Minutes, Self::Error> {
+        Ok(self
+            .device
+            .average_time_to_full()
+            .read_async()
+            .await?
+            .average_time_to_full())
+    }
+
+    async fn battery_status(&mut self) -> Result<smart_battery::BatteryStatusFields, Self::Error> {
+        let status: smart_battery::BatteryStatusFields = self.device.battery_status().read_async().await?.into();
+
+        match status.error_code() {
+            ErrorCode::Ok => Ok(status),
+            _ => Err(BQ40Z50Error::BatteryStatus(status.error_code())),
+        }
+    }
+
+    async fn cycle_count(&mut self) -> Result<smart_battery::Cycles, Self::Error> {
+        Ok(self.device.cycle_count().read_async().await?.cycle_count())
+    }
+
+    async fn design_capacity(&mut self) -> Result<smart_battery::CapacityModeValue, Self::Error> {
+        Ok(match self.capacity_mode_state.get() {
+            CapacityModeState::Milliamps => smart_battery::CapacityModeValue::MilliAmpUnsigned(
+                self.device.design_capacity().read_async().await?.design_capacity(),
+            ),
+            CapacityModeState::Centiwatt => smart_battery::CapacityModeValue::CentiWattUnsigned(
+                self.device.design_capacity().read_async().await?.design_capacity(),
+            ),
+        })
+    }
+
+    async fn design_voltage(&mut self) -> Result<smart_battery::MilliVolts, Self::Error> {
+        Ok(self.device.design_voltage().read_async().await?.design_voltage())
+    }
+
+    async fn specification_info(&mut self) -> Result<SpecificationInfoFields, Self::Error> {
+        Ok(self.device.specification_info().read_async().await?.into())
+    }
+
+    async fn manufacture_date(&mut self) -> Result<smart_battery::ManufactureDate, Self::Error> {
+        Ok(self
+            .device
+            .manufacture_date()
+            .read_async()
+            .await?
+            .manufacture_date()
+            .into())
+    }
+
+    async fn serial_number(&mut self) -> Result<u16, Self::Error> {
+        Ok(self.device.serial_number().read_async().await?.serial_number())
+    }
+
+    async fn manufacturer_name(&mut self, name: &mut [u8]) -> Result<(), Self::Error> {
+        self.device.manufacture_name().read_async(name).await.map(|_f| ())
+    }
+
+    async fn device_name(&mut self, name: &mut [u8]) -> Result<(), Self::Error> {
+        self.device.device_name().read_async(name).await.map(|_f| ())
+    }
+
+    async fn device_chemistry(&mut self, chemistry: &mut [u8]) -> Result<(), Self::Error> {
+        self.device.device_chemistry().read_async(chemistry).await.map(|_f| ())
     }
 }
 

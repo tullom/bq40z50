75f693f285c5e93a100cceef8561c9a97a933964
matteotullo <matteotullo@microsoft.com>
2025-05-16 10:43:07 -0700

    Break driver functionality into chip revisions and add initial support for R4
    - Updates CI to test the mutually exclusive features
    - Adds compile time checks to enforce mutual exclusion on the revision features
    - Also adds extra missing fields to the Cargo.toml

--- build.rs
+++ build.rs
@@ -1,4 +1,13 @@
 #![allow(missing_docs)]
 fn main() {
-    println!("cargo:rebuild-if-changed=device.yaml");
+    #[cfg(feature = "r1")]
+    println!("cargo:rebuild-if-changed=device_R1.yaml");
+    #[cfg(feature = "r4")]
+    println!("cargo:rebuild-if-changed=device_R4.yaml");
+
+    #[cfg(not(any(feature = "r1", feature = "r4")))]
+    compile_error!("Please enable a chip revision feature that corresponds to the one you are using!");
+
+    #[cfg(all(feature = "r1", feature = "r4"))]
+    compile_error!("Please enable only one chip revision that corresponds to the one you are using!");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -29,8 +29,11 @@ pub enum BQ40Z50Error<I2cError> {
 }
 
 const BQ_ADDR: u8 = 0x0B;
+#[cfg(any(feature = "r1", feature = "r4"))]
 const LARGEST_REG_SIZE_BYTES: usize = 5;
+#[cfg(any(feature = "r1", feature = "r4"))]
 const LARGEST_CMD_SIZE_BYTES: usize = 32;
+#[cfg(any(feature = "r1", feature = "r4"))]
 const LARGEST_BUF_SIZE_BYTES: usize = 33;
 const MAC_CMD_ADDR_SIZE_BYTES: usize = 2;
 const MAC_CMD_ADDR_SIZE_BITS: usize = MAC_CMD_ADDR_SIZE_BYTES * 8;
@@ -84,9 +87,16 @@ pub struct DeviceInterface<I2C: I2cTrait> {
     pub i2c: I2C,
 }
 
+#[cfg(feature = "r1")]
 device_driver::create_device!(
     device_name: Device,
-    manifest: "device.yaml"
+    manifest: "device_R1.yaml"
+);
+
+#[cfg(feature = "r4")]
+device_driver::create_device!(
+    device_name: Device,
+    manifest: "device_R4.yaml"
 );
 
 impl<I2C: I2cTrait> device_driver::AsyncRegisterInterface for DeviceInterface<I2C> {

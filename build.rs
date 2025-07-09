530bd84c240841edf4e801328a272cd9ba17b05d
matteotullo <matteotullo@microsoft.com>
2025-07-09 16:44:51 -0700

    Add r3 and r5, update r4

--- build.rs
+++ build.rs
@@ -2,12 +2,23 @@
 fn main() {
     #[cfg(feature = "r1")]
     println!("cargo:rebuild-if-changed=device_R1.yaml");
+    #[cfg(feature = "r3")]
+    println!("cargo:rebuild-if-changed=device_R3.yaml");
     #[cfg(feature = "r4")]
     println!("cargo:rebuild-if-changed=device_R4.yaml");
+    #[cfg(feature = "r5")]
+    println!("cargo:rebuild-if-changed=device_R5.yaml");
 
-    #[cfg(not(any(feature = "r1", feature = "r4")))]
+    #[cfg(not(any(feature = "r1", feature = "r3", feature = "r4", feature = "r5")))]
     compile_error!("Please enable a chip revision feature that corresponds to the one you are using!");
 
-    #[cfg(all(feature = "r1", feature = "r4"))]
+    #[cfg(any(
+        all(feature = "r1", feature = "r3"),
+        all(feature = "r1", feature = "r4"),
+        all(feature = "r1", feature = "r5"),
+        all(feature = "r3", feature = "r4"),
+        all(feature = "r3", feature = "r5"),
+        all(feature = "r4", feature = "r5"),
+    ))]
     compile_error!("Please enable only one chip revision that corresponds to the one you are using!");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -29,11 +29,11 @@ pub enum BQ40Z50Error<I2cError> {
 }
 
 // Gated as future revisions of this chip may have larger register sizes.
-#[cfg(any(feature = "r1", feature = "r4"))]
+#[cfg(any(feature = "r1", feature = "r3", feature = "r4", feature = "r5"))]
 const LARGEST_REG_SIZE_BYTES: usize = 5;
-#[cfg(any(feature = "r1", feature = "r4"))]
+#[cfg(any(feature = "r1", feature = "r3", feature = "r4", feature = "r5"))]
 const LARGEST_CMD_SIZE_BYTES: usize = 32;
-#[cfg(any(feature = "r1", feature = "r4"))]
+#[cfg(any(feature = "r1", feature = "r3", feature = "r4", feature = "r5"))]
 const LARGEST_BUF_SIZE_BYTES: usize = 33;
 
 const BQ_ADDR: u8 = 0x0Bu8;
@@ -105,10 +105,22 @@ device_driver::create_device!(
     manifest: "device_R1.yaml"
 );
 
+#[cfg(feature = "r3")]
+device_driver::create_device!(
+    device_name: Device,
+    manifest: "device_R3.yaml"
+);
+
 #[cfg(feature = "r4")]
 device_driver::create_device!(
     device_name: Device,
     manifest: "device_R4.yaml"
+);
+
+#[cfg(feature = "r5")]
+device_driver::create_device!(
+    device_name: Device,
+    manifest: "device_R5.yaml"
 );
 
 impl<I2C: I2cTrait> device_driver::AsyncRegisterInterface for DeviceInterface<I2C> {
